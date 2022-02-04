ba842f4ada7ee4a7ba0447cf0abe1942d3c841ad
Smitha <88016112+Smitha-4@users.noreply.github.com>
2022-02-04 16:25:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+  var ctx = document.getElementById("educationchart").getContext('2d');
+  var myChart1 = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Masters', 'Bachelors', 'Pre University', 'High School'],
+        datasets: [{
+            label: 'My Education CGPA',
+            data: [7.89,8,8.5,9.328],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var ctx = document.getElementById("skillschart").getContext('2d');
+  var myChart1 = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['web development', 'Data Mining', 'Exproratory Data Analysis', 'Machine Learning'],
+        datasets: [{
+            label: 'My skills',
+            data: [85,70,80,60],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
